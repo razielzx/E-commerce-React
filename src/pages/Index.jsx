12d7efe9c9edc65d869321ac5b6c19d60a357ed0
razielzx <98990811+razielzx@users.noreply.github.com>
2022-08-11 16:48:04 -0500

    update

--- src/pages/Index.jsx
+++
@@ -1,4 +0,0 @@
-export {default as Home} from './Home';
-export {default as Purchases} from './Purchases'
-export {default as Login} from './Login'
-export {default as ProductDetail} from './ProductDetail'
